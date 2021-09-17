7a29c6e5eeafa57529bb26ed8a493cdcd4cab943
Sokolov Innokenty <im@qbbr.io>
2021-09-17 17:08:06 +0800

    fix ./path/to/tray-screenshot-tools.py

--- const.py
+++ const.py
@@ -1,7 +1,7 @@
 import os
 
 TRAY_TOOLTIP = 'tray-screenshot-tools by @qbbr'
-TRAY_ICON = os.path.curdir + '/tray-icon.png'
-SCREENSHOTS_DIR = os.path.curdir + '/screenshots'
+TRAY_ICON = os.path.dirname(__file__) + '/tray-icon.png'
+SCREENSHOTS_DIR = os.path.dirname(__file__) + '/screenshots'
 COPY_TO_CLIPBOARD = False
 OPEN_IMAGE = False
